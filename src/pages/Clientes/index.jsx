55d1f9fea7e919811be1a7e5f4c6148268e5524d
kaiudiass <kaiod629@gmail.com>
2025-02-23 01:24:17 -0300

    fix: add ContainerAdjustment import on clients

--- src/pages/Clientes/index.jsx
+++ src/pages/Clientes/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import MenuDesktop from '../../components/MenuResize/MenuDesktop'
+import ContainerAdjustment from '../../components/ContainerAdjustment'
 
 const Clientes = () => {
   return (
     <div>
         <MenuDesktop/>
         <ContainerAdjustment>
-            
+
         </ContainerAdjustment>
     </div>
   )
