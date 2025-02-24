7ca1f19f1c1b023010338feca3971f3d01285090
kaiudiass <kaiod629@gmail.com>
2025-02-23 22:52:39 -0300

    feat: add component HeaderDesktop e ButtonContainer in Clientes

--- src/pages/Clientes/index.jsx
+++ src/pages/Clientes/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import MenuDesktop from '../../components/MenuResize/MenuDesktop'
 import ContainerAdjustment from '../../components/ContainerAdjustment'
+import HeaderDesktop from '../../components/HeaderResize/HeaderDesktop'
+import ButtonContainer from './ButtonContainer'
 
 const Clientes = () => {
   return (
     <div>
         <MenuDesktop/>
         <ContainerAdjustment>
+        <HeaderDesktop/>
+        <ButtonContainer/>
 
         </ContainerAdjustment>
     </div>
