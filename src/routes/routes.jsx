d0c51a7cd1c67d6f86b6c58792def4cbd210866f
kaiudiass <kaiod629@gmail.com>
2025-02-26 17:24:11 -0300

    fix: I changed the profile route

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -3,6 +3,7 @@ import { Routes, Route} from 'react-router-dom';
 import Home from '../pages/Home';
 import Clientes from '../pages/Clientes';
 import Loans from '../pages/Loans';
+import Profile from '../pages/Profile';
 
 
 const AppRoutes = () => {
@@ -10,6 +11,7 @@ const AppRoutes = () => {
    
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/profile" element={<Profile/>} />
         <Route path="/clientes" element={<Clientes/>} />
         <Route path="/emprestimo" element={<Loans/>} />
         <Route path="/3" element={<Home/>} />
