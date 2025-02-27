ef231bc55ebcebfbed9bd0f314c0ebb0192330b8
kaiudiass <kaiod629@gmail.com>
2025-02-26 23:13:38 -0300

    feat: add label in form

--- src/pages/Profile/ContainerInput/index.jsx
+++ src/pages/Profile/ContainerInput/index.jsx
@@ -6,32 +6,32 @@ const ContainerInput = () => {
     <form>
             <div className={styles.formGroup}>
                 <div className={styles.inputPair}>
-                    <div>
-                       
+                    <div className={styles.inputContainer}>
+                        <label>Nome</label>
                         <input type="text"  placeholder="Kaio" className={styles.inputField} />
                     </div>
-                    <div>
-                    
+                    <div className={styles.inputContainer}>
+                    <label>Sobrenome</label>
                         <input type="text"  placeholder="Dias" className={styles.inputField} />
                     </div>
                 </div>
                 <div className={styles.inputPair}>
-                    <div>
-                        
+                    <div className={styles.inputContainer}>
+                    <label>Cidade</label>
                         <input type="text"  placeholder="Divisópolis" className={styles.inputField} />
                     </div>
-                    <div>
-                        
+                    <div className={styles.inputContainer}>
+                    <label>Endereço</label>
                         <input type="text"   placeholder="Eudes Mendes 112" className={styles.inputField} />
                     </div>
-                </div>
+                </div >
                 <div className={styles.inputPair}>
-                    <div>
-                      
+                    <div className={styles.inputContainer}>
+                        <label>Email</label>
                         <input type="email"  placeholder="kaiod629@gmail.com" className={styles.inputField} />
                     </div>
-                    <div>
-                       
+                    <div className={styles.inputContainer}>
+                    <label>Senha</label>
                         <input type="password"  placeholder="Digite sua nova senha"  className={styles.inputField} />
                     </div>
                 </div>
