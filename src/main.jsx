23917be1455caf905d6f4d9f9ab18868dc829ddb
kaiudiass <kaiod629@gmail.com>
2025-02-27 02:13:54 -0300

    feat: add FormProfileProvider in Main

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { MenuProvider } from './Context/MenuContext.jsx'
+import { FormProfileProvider } from './Context/FormProfileContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <FormProfileProvider>
     <MenuProvider>
     <App />
     </MenuProvider>
+    </FormProfileProvider>
   </StrictMode>,
 )
