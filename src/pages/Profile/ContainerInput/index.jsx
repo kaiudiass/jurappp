11327dc994e4a7830b0f3560ff5aa6b20e76ca04
kaiudiass <kaiod629@gmail.com>
2025-02-27 02:16:10 -0300

    fix: changes to profile inputs

--- src/pages/Profile/ContainerInput/index.jsx
+++ src/pages/Profile/ContainerInput/index.jsx
@@ -1,38 +1,49 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import styles from './ContainerInput.module.css';
+import FormProfileContext from '../../../Context/FormProfileContext';
 
 const ContainerInput = () => {
+
+   const {isEditing, setIsEditing,formData, setFormData, userInfo, handleSave,handleEditClick ,handleChange, handleSaveClick } =  useContext(FormProfileContext)
+
+
   return (
     <form>
             <div className={styles.formGroup}>
                 <div className={styles.inputPair}>
                     <div className={styles.inputContainer}>
                         <label>Nome</label>
-                        <input type="text"  placeholder="Kaio" className={styles.inputField} />
+                        <input type="text"  name="Name"  placeholder={userInfo.Name} className={styles.inputField}   onChange={handleChange}
+                     disabled={!isEditing} value={formData.Name} />
                     </div>
                     <div className={styles.inputContainer}>
                     <label>Sobrenome</label>
-                        <input type="text"  placeholder="Dias" className={styles.inputField} />
+                        <input type="text" name="Sobrenome"  placeholder={userInfo.Sobrenome} className={styles.inputField}  onChange={handleChange}
+                    disabled={!isEditing}  value={formData.Sobrenome} />
                     </div>
                 </div>
                 <div className={styles.inputPair}>
                     <div className={styles.inputContainer}>
                     <label>Cidade</label>
-                        <input type="text"  placeholder="Divisópolis" className={styles.inputField} />
+                        <input type="text"   name="Cidade" placeholder={userInfo.Cidade} className={styles.inputField} 
+                    onChange={handleChange} disabled={!isEditing} value={formData.Cidade}/>
                     </div>
                     <div className={styles.inputContainer}>
                     <label>Endereço</label>
-                        <input type="text"   placeholder="Eudes Mendes 112" className={styles.inputField} />
+                        <input type="text" name="Endereço"  placeholder={userInfo.Endereço} className={styles.inputField} 
+                    onChange={handleChange} disabled={!isEditing}   value={formData.Endereço}/>
                     </div>
                 </div >
                 <div className={styles.inputPair}>
                     <div className={styles.inputContainer}>
                         <label>Email</label>
-                        <input type="email"  placeholder="kaiod629@gmail.com" className={styles.inputField} />
+                        <input type="email"  name="Email" placeholder={userInfo.Email} className={styles.inputField}    
+                    onChange={handleChange} disabled={!isEditing} value={formData.Email}/>
                     </div>
                     <div className={styles.inputContainer}>
                     <label>Senha</label>
-                        <input type="password"  placeholder="Digite sua nova senha"  className={styles.inputField} />
+                        <input type="password" name="Senha" placeholder="Digite sua nova senha"  className={styles.inputField} 
+                        onChange={handleChange}disabled={!isEditing} value={formData.Senha} />
                     </div>
                 </div>
             </div>
